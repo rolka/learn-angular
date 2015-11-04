24d0913e1f7d4c00d57273c6bfc2ab2f02a2523d
Rolandas <rzabulis@yahoo.co.uk>
2015-11-04 15:30:36 +0200

    second test done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,3 +9,21 @@ angular.module('myApp.controllers', []).
   .controller('MyCtrl2', [function() {
 
   }]);
+
+//
+
+angular.module( 'myApp.controllers', [] ).controller('FinanceController', function( $scope ){
+
+  $scope.salary = 0;
+  $scope.percentage = 0;
+  $scope.result = function(){
+    return $scope.salary * $scope.percentage * 0.01;
+  };
+
+});
+
+
+
+
+
+
